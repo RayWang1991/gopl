afe64f3dfbe472818a1f0190a957434dd253833f
ray wang <wangrui@bongmi.com>
2017-08-30 10:57:08 +0800

    count words

--- src/ch4/ex4.9/countWord.go
+++ src/ch4/ex4.9/countWord.go
@@ -1,1 +1,25 @@
-package ex4_9
+package main
+
+import (
+	"io"
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	countWord(os.Stdin)
+}
+
+func countWord(reader io.Reader) {
+	counts := map[string]int{}
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		text := scanner.Text()
+		counts[text]++
+	}
+	for k, v := range counts {
+		fmt.Printf("word: %q, fre: %d\n", k, v)
+	}
+}
