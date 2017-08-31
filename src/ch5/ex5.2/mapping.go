cb726c056735f37e7a0903b275b8a43a56bdcc0f
ray wang <wangrui@bongmi.com>
2017-08-31 09:57:32 +0800

    mapping html element nodes

--- src/ch5/ex5.2/mapping.go
+++ src/ch5/ex5.2/mapping.go
@@ -1,1 +1,31 @@
-package ex5_2
+package main
+
+import (
+	"golang.org/x/net/html"
+	"os"
+	"log"
+	"fmt"
+)
+
+func main() {
+	root, err := html.Parse(os.Stdin)
+	if err != nil {
+		log.Fatal("Error on parsing from Stdin %v", err)
+	}
+	count := map[string]int{}
+	traversePreorder(root, count)
+	for k, v := range count {
+		fmt.Printf("%-6.6s  #%d\n", k, v)
+	}
+}
+
+func traversePreorder(n *html.Node, counts map[string]int) {
+	if n == nil || n.Type == html.ErrorNode {
+		return
+	}
+	if n.Type == html.ElementNode {
+		counts[n.Data]++
+	}
+	traversePreorder(n.NextSibling, counts)
+	traversePreorder(n.FirstChild, counts)
+}
