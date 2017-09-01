610e17fcb617de405ce02814625bdd8bddeeed52
ray wang <wangrui@bongmi.com>
2017-09-01 10:40:58 +0800

    update

--- src/ch5/outline2/outline.go
+++ src/ch5/outline2/outline.go
@@ -1,1 +1,38 @@
 package outline2
+
+import (
+	"golang.org/x/net/html"
+	"fmt"
+)
+
+func Outline2(node *html.Node) {
+	forEachNode(node, startElement, endElement)
+}
+
+func forEachNode(node *html.Node, pre, post func(*html.Node)) {
+	if pre != nil {
+		pre(node)
+	}
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(node)
+	}
+}
+
+var depth int = 0
+
+func startElement(node *html.Node) {
+	if node.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", node.Data)
+	}
+	depth++
+}
+
+func endElement(node *html.Node) {
+	depth--
+	if node.Type == html.ElementNode {
+		fmt.Printf("%*s</%s>\n", depth*2, "", node.Data)
+	}
+}
