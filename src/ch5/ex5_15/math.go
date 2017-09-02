f1290e4310294ced2bf4ecfc8af62aab9a7456bc
ray wang <wangrui@bongmi.com>
2017-09-02 18:31:18 +0800

    min and max

--- src/ch5/ex5_15/math.go
+++ src/ch5/ex5_15/math.go
@@ -1,1 +1,28 @@
-package ex5_15
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// let it panic
+	fmt.Printf("Min: %d\nMax: %d\n", Min(1, 2, 5), Max(8, 3, 1))
+}
+
+func Min(arg int, args ...int) int {
+	for _, a := range args {
+		if a < arg {
+			arg = a
+		}
+	}
+	return arg
+}
+
+func Max(arg int, args ...int) int {
+	for _, a := range args {
+		if a > arg {
+			arg = a
+		}
+	}
+	return arg
+}
