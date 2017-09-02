36b8f8b31593a4204d2fffbb04632b378ca7854c
ray wang <wangrui@bongmi.com>
2017-09-03 00:22:45 +0800

    trace using defer, defer tricks for triple

--- src/ch5/trace/trace.go
+++ src/ch5/trace/trace.go
@@ -1,1 +1,18 @@
-package gopl
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	defer trace("operation")()
+	time.Sleep(10 * time.Second)
+}
+
+func trace(msg string) func() {
+	now := time.Now()
+	return func() {
+		fmt.Printf("%s start: %v end:%v\n", msg, now, time.Now())
+	}
+}
