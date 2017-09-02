c2a9d9dfee21017299c9c21a9d5c4475d7430e3b
ray wang <wangrui@bongmi.com>
2017-09-03 00:22:58 +0800

    defer tricks

--- src/ch5/deferTricks/deferTricks.go
+++ src/ch5/deferTricks/deferTricks.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func double(x int) (result int) {
+	defer func() {
+		fmt.Printf("doubld(%d) = %d \n", x, result)
+	}()
+	return x + x
+}
+
+func triple(x int) (result int) {
+	defer func() {
+		result += x
+	}()
+	return double(x)
+}
+
+func main() {
+	x := 5
+	double(x)
+	res := triple(x)
+	fmt.Printf("The result of triple(%d) is %d", x, res)
+}
--- src/ch5/fetch/fetch.go
+++ src/ch5/fetch/fetch.go
@@ -1,1 +1,32 @@
 package fetch
+
+import (
+	"net/http"
+	"path"
+	"os"
+	"io"
+)
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	// Close file, but prefer error from copy, if any
+	// (For NFS like file systems report write errors till the file is closed)
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+}
