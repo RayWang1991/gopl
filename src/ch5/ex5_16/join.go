31cfa3ec6387cd371d2be4154338822e52847c73
ray wang <wangrui@bongmi.com>
2017-09-02 18:31:36 +0800

    String.Join variadic

--- src/ch5/ex5_16/join.go
+++ src/ch5/ex5_16/join.go
@@ -1,1 +1,14 @@
 package ex5_16
+
+import "bytes"
+
+func Join(sep string, strs ...string) string {
+	s := bytes.Buffer{}
+	for i, str := range strs {
+		if i > 0 {
+			s.WriteString(sep)
+		}
+		s.WriteString(str)
+	}
+	return s.String()
+}
