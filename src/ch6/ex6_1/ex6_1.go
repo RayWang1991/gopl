5c7f2a39afc97bd8e73ca958a25d359419d120ca
ray wang <wangrui@bongmi.com>
2017-09-03 15:52:57 +0800

    make([]T ,len), copy only copy the min len

---
+++ src/ch6/ex6_1/ex6_1.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	if l := len(s.words); word >= l {
+		s.words = append(s.words, make([]uint64, word+1-l)...)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) Union(t *IntSet) {
+	n, m := len(s.words), len(t.words)
+	var min = m
+	if n < m {
+		s.words = append(s.words, t.words[n:]...)
+		min = n
+	}
+	for i := 0; i < min; i++ {
+		s.words[i] |= t.words[i]
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word != 0 {
+			for j := 0; j < 64; j ++ {
+				if word&(1<<uint(j)) != 0 {
+					if buf.Len() > len("{") {
+						buf.WriteByte(' ')
+					}
+					fmt.Fprintf(&buf, "%d", 64*i+j)
+				}
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	var s IntSet
+	s.Add(0)
+	s.Add(7)
+	s.Add(3)
+	fmt.Println(s.Len())
+	s.Remove(3)
+	fmt.Println(&s)
+	t := s.Copy()
+	fmt.Println(t)
+	fmt.Println(t == &s)
+}
+
+// return the number of elements
+func (s *IntSet) Len() (res int) {
+	for _, word := range s.words {
+		for ; word != 0; word &= word - 1 {
+			res ++
+		}
+	}
+	return res
+}
+
+// remove x from the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+// remove all elements from the set
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+// return a copy of te set
+func (s *IntSet) Copy() *IntSet {
+	w := make([]uint64,len(s.words))
+	copy(w, s.words)
+	return &IntSet{w}
+}
