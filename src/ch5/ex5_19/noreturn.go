45df8c4ec4b7ffdb985c1f5198234376b6fa3811
ray wang <wangrui@bongmi.com>
2017-09-03 15:23:44 +0800

    no return using panic

--- src/ch5/ex5_19/noreturn.go
+++ src/ch5/ex5_19/noreturn.go
@@ -1,1 +1,15 @@
-package ex5_19
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(f1())
+}
+
+func f1() (res string) {
+	defer func() {
+		recover()
+		res = "non empty"
+	}()
+	panic(1)
+}
