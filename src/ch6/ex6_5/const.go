74ed4b929dbd142ca7041c13e36dcd487062fadb
ray wang <wangrui@bongmi.com>
2017-09-03 18:25:37 +0800

    const using 32<<(^uinit(0)>>63)

---
+++ src/ch6/ex6_5/const.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint
+}
+
+const LenthForBit = 32 << (^uint(0) >> 63)
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/LenthForBit, uint(x%LenthForBit)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/LenthForBit, uint(x%LenthForBit)
+	if l := len(s.words); word >= l {
+		s.words = append(s.words, make([]uint, word+1-l)...)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) Union(t *IntSet) {
+	n, m := len(s.words), len(t.words)
+	var min = m
+	if n < m {
+		s.words = append(s.words, t.words[n:]...)
+		min = n
+	}
+	for i := 0; i < min; i++ {
+		s.words[i] |= t.words[i]
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word != 0 {
+			for j := 0; j < LenthForBit; j ++ {
+				if word&(1<<uint(j)) != 0 {
+					if buf.Len() > len("{") {
+						buf.WriteByte(' ')
+					}
+					fmt.Fprintf(&buf, "%d", LenthForBit*i+j)
+				}
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	t, s := &IntSet{}, &IntSet{}
+	t.AddAll(0, 3, 7)
+	s.AddAll(0, 1)
+	fmt.Println(t.Elems())
+}
+
+// return the number of elements
+func (s *IntSet) Len() (res int) {
+	for _, word := range s.words {
+		for ; word != 0; word &= word - 1 {
+			res ++
+		}
+	}
+	return res
+}
+
+// remove x from the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/LenthForBit, uint(x%LenthForBit)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+// remove all elements from the set
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+// return a copy of te set
+func (s *IntSet) Copy() *IntSet {
+	w := make([]uint, len(s.words))
+	copy(w, s.words)
+	return &IntSet{w}
+}
+
+// Add all from the args
+func (s *IntSet) AddAll(args ... int) {
+	for _, a := range args {
+		s.Add(a)
+	}
+}
+
+// IntersectWith
+func (s *IntSet) IntersectWith(t *IntSet) {
+	m := len(s.words)
+	if n := len(t.words); n < m {
+		m = n
+	}
+	for i := 0; i < m; i ++ {
+		s.words[i] &= t.words[i]
+	}
+	s.words = s.words[:m]
+}
+
+// DifferenceWith
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, w := range s.words {
+		if len(t.words) > i {
+			s.words[i] = w &^ t.words[i]
+		}
+	}
+}
+
+// SymetricDifferenceWith
+func (s *IntSet) SymetricDifferenceWith(t *IntSet) {
+	for i, w := range t.words {
+		if len(s.words) > i {
+			s.words[i] ^= w
+		} else {
+			s.words = append(s.words, w)
+		}
+	}
+}
+
+// Elems return a slice containing the elements of the set
+func (s *IntSet) Elems() []uint {
+	res := []uint{}
+	for i, w := range s.words {
+		for ; w != 0; w &= w - 1 {
+			// cont j
+			c, j := w^(w-1), -1
+			for ; c != 0; c >>= 1 {
+				j++
+			}
+			res = append(res, uint(i*LenthForBit+j))
+		}
+	}
+	return res
+}
