1c3876f374d10679a48d477aa5f40c70c2134f41
ray wang <wangrui@bongmi.com>
2017-09-09 10:59:46 +0800

    reverb1 reversed

--- src/ch8/reverb1/reverb.go
+++ src/ch8/reverb1/reverb.go
@@ -20,7 +20,7 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 func handleConn(c net.Conn) {
 	input := bufio.NewScanner(c)
 	for input.Scan() {
-		go echo(c, input.Text(), 1*time.Second)
+		echo(c, input.Text(), 1*time.Second)
 	}
 	c.Close()
 }
--- src/ch8/testChan/testChan.go
+++ src/ch8/testChan/testChan.go
@@ -1,1 +1,33 @@
-package testChan
+package main
+
+import "fmt"
+
+
+func main(){
+	case1()
+	case2()
+}
+
+func case2() {
+
+}
+
+func case1() {
+	ch := make(chan int, 8) // buffered channel with capacity 8
+	for i := 0; i < 5; i++ {
+		ch <- i
+	}
+	for i := 0; i < 3; i ++ {
+		x := <-ch
+		fmt.Println(x)
+	}
+	close(ch)
+	// sending values to closed channel cause panic
+	// ch <- 9348
+	// receiving values from closed channel is allowed
+	// when there are no values left, empty values of that type will be receiving
+	for i := 0; i < 3; i ++ {
+		x := <-ch
+		fmt.Println(x)
+	}
+}
