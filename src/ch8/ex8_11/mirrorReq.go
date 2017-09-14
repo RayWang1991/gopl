5e7fa26b036146d4f25bf24b2c1d25685d85f684
ray wang <wangrui@bongmi.com>
2017-09-14 15:21:21 +0800

    test goroutine block

--- src/ch8/ex8_11/mirrorReq.go
+++ src/ch8/ex8_11/mirrorReq.go
@@ -1,1 +1,62 @@
-package ex8_11
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"os"
+	"runtime"
+	"sync"
+)
+
+type info struct {
+	header http.Header
+	host   string
+	err    error
+}
+
+var done = make(chan struct{})
+
+var wg = sync.WaitGroup{}
+
+// mirrorReq request a list of urls in parallel, return the fastest one, and cancel the others
+func main() {
+	// canceler
+	urls := os.Args[1:]
+	if len(urls) == 0 {
+		fmt.Printf("usage: enter urls\n")
+		return
+	}
+
+	res := make(chan *info , len(urls))
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			head, host, err := request(url)
+			res <- &info{head, host, err}
+			wg.Done()
+		}(url)
+	}
+	info := <-res
+	// cancel the others, and wait they are all closed
+	close(done)
+	fmt.Printf("cancel\n")
+	wg.Wait()
+	fmt.Printf("host:%s error:%s\n", info.host, info.err)
+	buf := make([]byte, 1<<16)
+	runtime.Stack(buf, true)
+	fmt.Printf("%s", buf)
+}
+
+func request(hostname string) (header http.Header, host string, err error) {
+	req, err := http.NewRequest("GET", hostname, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	req.Cancel = done
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, "", fmt.Errorf("getting:%s %s", req, err)
+	}
+	resp.Body.Close()
+	return resp.Header, resp.Request.URL.String(), nil
+}
--- src/ch8/testLeak1/leak.go
+++ src/ch8/testLeak1/leak.go
@@ -1,1 +1,34 @@
-package testLeak1
+package main
+
+import (
+	"time"
+	"fmt"
+	"runtime"
+)
+
+// create 3 goroutines, each of them send a message to a channel after a certain time
+// the main goroutine wait to receive value from that channel
+// test there is any goroutine is blocked
+
+func main() {
+	test()
+	panic(nil)
+}
+
+func test() {
+	now := time.Now()
+	done := make(chan struct{})
+	for i := 1; i < 4; i++ {
+		go func(i int) {
+			for j := 0; j < i*1000000000; j++ {
+			}
+			done <- struct{}{}
+		}(i)
+	}
+	<-done
+	time.Sleep(time.Duration(2) * time.Second)
+	fmt.Printf("%.1fs pased\n", time.Since(now).Seconds())
+	buf := make([]byte, 1<<16)
+	runtime.Stack(buf, true)
+	fmt.Printf("%s", buf)
+}
