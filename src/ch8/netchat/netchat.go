484c23fd2f7c1dd3b70d4be0484e856093858989
ray wang <wangrui@bongmi.com>
2017-09-15 19:36:49 +0800

    update net chat close reader and writer seperately

--- src/ch8/netchat/netchat.go
+++ src/ch8/netchat/netchat.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"log"
 	"io"
+	"fmt"
 )
 
 func main() {
@@ -15,14 +16,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	go mustCopy(os.Stdout, conn)
-	mustCopy(conn, os.Stdin)
+	tcpC := conn.(*net.TCPConn) // let it panic if
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // ignore potential io errors
+		fmt.Println("done\n")
+		done <- struct{}{}
+	}()
+	mustCopy(tcpC, os.Stdin)
+	tcpC.CloseWrite()
+	fmt.Println("eof done\n")
+	<-done
+	fmt.Println("close connect\n")
+	tcpC.CloseRead()
 }
 
 func mustCopy(writer io.Writer, reader io.Reader) {
 	_, err := io.Copy(writer, reader)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
