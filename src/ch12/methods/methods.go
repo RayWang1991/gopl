8b83db307d107593b1ab595fce2c5eed31c914f4
ray wang <wangrui@bongmi.com>
2017-09-21 09:52:13 +0800

    methods

--- src/ch12/methods/methods.go
+++ src/ch12/methods/methods.go
@@ -1,1 +1,24 @@
-package methods
+package main
+
+import (
+	"reflect"
+	"fmt"
+	"os"
+)
+
+// Print prints the methods set of the value x
+func Print(x interface{}){
+	v := reflect.ValueOf(x)
+	t := v.Type()
+	fmt.Printf("type %s\n",t)
+
+	for i:= 0; i< v.NumMethod(); i++{
+		methType := v.Method(i).Type()
+		fmt.Printf("%s | %s\n",t.Method(i).Name,methType.String())
+	}
+}
+
+func main(){
+	var i interface{} = os.Stdin
+	Print(i)
+}
